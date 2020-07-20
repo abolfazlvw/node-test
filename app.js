71616598ea3ed4338ac85f0a2b3a13787cc10a46
zwsq <abolemail@gmail.com>
2020-07-20 19:13:20 +0430

    first express server

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 var express = require('express');
 
+var app = express();
+var port = 1448;
 
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', function(req, res, next){
+    console.log('get 1');
+    res.sendFile(__dirname + '/static/home.html')
+})
+
+app.get('/login', function(req, res, next){
+    res.sendFile(__dirname + '/static/login.html') 
+})
+
+
+app.listen(port);
+console.error('Your server is runnign on port ' + port);
